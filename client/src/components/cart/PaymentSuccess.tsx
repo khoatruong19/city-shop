01c8b6c9805b6e1a4015fa73c05dc776e8b85c3f
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2022-10-07 17:42:08 +0700

    payment success page

--- client/src/components/cart/PaymentSuccess.tsx
+++ client/src/components/cart/PaymentSuccess.tsx
@@ -1,7 +1,29 @@
+import { CheckCircleIcon } from '@heroicons/react/24/solid';
+import { Button, Group, Stack, Title } from '@mantine/core';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { mainOrangeColor } from '../../utils/constants';
 
 const PaymentSuccess = () => {
-  return <div>PaymentSuccess</div>;
+  const navigate = useNavigate();
+  return (
+    <Group
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Stack align={'center'}>
+        <CheckCircleIcon style={{ color: mainOrangeColor, width: '8rem' }} />
+        <Title order={2}>Your Order has been placed successfully</Title>
+        <Button size="lg" color={'orange'} onClick={() => navigate('/orders')}>
+          View Orders
+        </Button>
+      </Stack>
+    </Group>
+  );
 };
 
 export default PaymentSuccess;
